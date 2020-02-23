11b5b63d48943c14a5276e97a3e62b570520608e
BigBoyBonanza <47259396+BigBoyBonanza@users.noreply.github.com>
2020-02-22 21:25:55 -0800

    PlayerController and beginnings of data reading

---
+++ BulletHellGame/Assets/Scripts/Composer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+using System.Linq;
+
+public class Composer : MonoBehaviour
+{
+    public TextAsset asset;
+    private string assetContent;
+    public static List<string> textArray;
+
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+        StartCoroutine(Loop());
+        
+    }
+
+    private void Start()
+    {
+        assetContent = asset.text;
+        ReadTheLines();
+    }
+
+    // Increase the number of calls to Update.
+    void Update()
+    {
+    }
+
+    // Increase the number of calls to FixedUpdate.
+    void FixedUpdate()
+    {
+
+    }
+
+    IEnumerator Loop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            
+        }
+    }
+
+    private void ReadTheLines()
+    {
+        textArray = assetContent.Split('\n').ToList();
+        for (int i = 0; i < 60; i++)
+        {
+            Debug.Log(textArray[i]);
+        }
+        //Debug.Log(asset.text);
+    }
+}
---
+++ BulletHellGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float distance = 36.0f;
+
+    /*
+    private Rigidbody rb;
+    private float moveSpeed = 100f;
+    public Vector3 direction;
+    */
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = distance;
+        transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+        /*
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = distance;
+            direction = (mousePosition - transform.position).normalized;
+            rb.velocity = new Vector3(direction.x * moveSpeed, mousePosition.z, direction.y * moveSpeed);
+            //transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+        }
+
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+        */
+    }
+}
