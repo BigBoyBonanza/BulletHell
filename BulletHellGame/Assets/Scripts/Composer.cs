7ed7d0f2b2f5fed6357687cacea0e79fc44b20e7
BigBoyBonanza <47259396+BigBoyBonanza@users.noreply.github.com>
2020-02-25 20:23:01 -0800

    stabilizing

--- BulletHellGame/Assets/Scripts/Composer.cs
+++ BulletHellGame/Assets/Scripts/Composer.cs
@@ -12,9 +12,29 @@ public class Composer : MonoBehaviour
     public static List<string> textArray;
     public int frameIndex = 0;
 
+    public int target = 60;
+
+    public GameObject deathBall;
+
+    public AudioSource song;
+    int countDown = 3;
+    bool cleared = false;
+
+    public GameObject spawner0;
+    public GameObject spawner1;
+    public GameObject spawner2;
+    public GameObject spawner3;
+    public GameObject spawner4;
+    public GameObject spawner5;
+    public GameObject spawner6;
+    public GameObject spawner7;
+    public GameObject spawner8;
+    public GameObject spawner9;
+
     void Awake()
     {
-        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = target;
         StartCoroutine(Loop());
         
     }
@@ -23,18 +43,27 @@ public class Composer : MonoBehaviour
     {
         assetContent = asset.text;
         //ReadTheLines();
+        Debug.Log(spawner0.transform.position);
     }
 
     // Increase the number of calls to Update.
     void Update()
     {
+        if(target != Application.targetFrameRate)
+        {
+            Application.targetFrameRate = target;
+        }
     }
 
     // Increase the number of calls to FixedUpdate.
     void FixedUpdate()
     {
-        ReadALine(frameIndex);
-        frameIndex++;
+        if(cleared)
+        {
+            ReadALine(frameIndex);
+            frameIndex++;
+        }
+
     }
 
     IEnumerator Loop()
@@ -42,7 +71,12 @@ public class Composer : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(1);
-            
+            countDown--;
+            if(countDown == 0)
+            {
+                cleared = true;
+                song.Play();
+            }
         }
     }
 
@@ -74,47 +108,97 @@ public class Composer : MonoBehaviour
         for (int j = 0; j < size; j++)
         {
             float num = float.Parse(textLine[j]);
-            Debug.Log(num);
+            //Debug.Log(num);
             //total += num;
             if(j == 0)
             {
-
+                if(num > 2)
+                {
+                    GameObject doomSphere = Instantiate(deathBall, spawner0.transform);
+                    Rigidbody rb = doomSphere.GetComponent<Rigidbody>();
+                    rb.AddForce(spawner0.transform.forward * 50f, ForceMode.VelocityChange);
+                }
             }
             if (j == 1)
             {
-
+                if (num > 5)
+                {
+                    GameObject doomSphere = Instantiate(deathBall, spawner1.transform);
+                    Rigidbody rb = doomSphere.GetComponent<Rigidbody>();
+                    rb.AddForce(spawner1.transform.forward * 50f, ForceMode.VelocityChange);
+                }
             }
             if (j == 2)
             {
-
+                if (num > 5)
+                {
+                    GameObject doomSphere = Instantiate(deathBall, spawner2.transform);
+                    Rigidbody rb = doomSphere.GetComponent<Rigidbody>();
+                    rb.AddForce(spawner2.transform.forward * 50f, ForceMode.VelocityChange);
+                }
             }
             if (j == 3)
             {
-
+                if (num > 5)
+                {
+                    GameObject doomSphere = Instantiate(deathBall, spawner3.transform);
+                    Rigidbody rb = doomSphere.GetComponent<Rigidbody>();
+                    rb.AddForce(spawner3.transform.forward * 50f, ForceMode.VelocityChange);
+                }
             }
             if (j == 4)
             {
-
+                if (num > 5)
+                {
+                    GameObject doomSphere = Instantiate(deathBall, spawner4.transform);
+                    Rigidbody rb = doomSphere.GetComponent<Rigidbody>();
+                    rb.AddForce(spawner4.transform.forward * 50f, ForceMode.VelocityChange);
+                }
             }
             if (j == 5)
             {
-
+                if (num > 5)
+                {
+                    GameObject doomSphere = Instantiate(deathBall, spawner5.transform);
+                    Rigidbody rb = doomSphere.GetComponent<Rigidbody>();
+                    rb.AddForce(spawner5.transform.forward * 50f, ForceMode.VelocityChange);
+                }
             }
             if (j == 6)
             {
-
+                if (num > 5)
+                {
+                    GameObject doomSphere = Instantiate(deathBall, spawner6.transform);
+                    Rigidbody rb = doomSphere.GetComponent<Rigidbody>();
+                    rb.AddForce(spawner6.transform.forward * 50f, ForceMode.VelocityChange);
+                }
             }
             if (j == 7)
             {
-
+                if (num > 5)
+                {
+                    GameObject doomSphere = Instantiate(deathBall, spawner7.transform);
+                    Rigidbody rb = doomSphere.GetComponent<Rigidbody>();
+                    rb.AddForce(spawner7.transform.forward * 50f, ForceMode.VelocityChange);
+                }
             }
             if (j == 8)
             {
-
+                if (num > 5)
+                {
+                    GameObject doomSphere = Instantiate(deathBall, spawner8.transform);
+                    Rigidbody rb = doomSphere.GetComponent<Rigidbody>();
+                    rb.AddForce(spawner8.transform.forward * 50f, ForceMode.VelocityChange);
+                }
             }
             if (j == 9)
             {
-
+                if (num > 5)
+                {
+                    GameObject doomSphere = Instantiate(deathBall, spawner9.transform);
+                    Rigidbody rb = doomSphere.GetComponent<Rigidbody>();
+                    rb.AddForce(spawner9.transform.forward * 50f, ForceMode.VelocityChange);
+                }
             }
         }
     }
