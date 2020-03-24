ab65f15de2b35304cfcc04aaa1b9eed6eeaa3572
BigBoyBonanza <47259396+BigBoyBonanza@users.noreply.github.com>
2020-03-24 11:50:50 -0700

    more dangers

---
+++ BulletHellGame/Assets/Scripts/StarSpawningScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSpawningScript : MonoBehaviour {
+
+    public GameObject[] Spawners;
+
+    public GameObject Star;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void ChoseSpawner()
+    {
+
+    }
+
+    public void Launch(GameObject spawner)
+    {
+        GameObject star = Instantiate(Star, spawner.transform.position, spawner.transform.rotation);
+        Rigidbody rb = star.GetComponent<Rigidbody>();
+        rb.AddForce(spawner.transform.forward * 40f, ForceMode.VelocityChange);
+    }
+}
